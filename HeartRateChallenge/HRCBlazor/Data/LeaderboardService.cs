7371269dae4506c212ffe3e41c94d209dab491d5
Blake-VM <Blake-VM@DESKTOP-N7ID9G2>
2020-08-07 13:03:36 -0700

    Init Commit for HRCBlazor

---
+++ HeartRateChallenge/HRCBlazor/Data/LeaderboardService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRCDB.Data.HRC;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace HRCBlazor.Data
+{
+    public class LeaderboardService
+    {
+        private readonly HRCContext _context;
+
+        public LeaderboardService(HRCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Leaderboard[]> GetLeaderboardsAsync()
+        {
+            Leaderboard[] data =
+                _context.Leaderboard
+                .FromSqlRaw($"EXECUTE sp_SelectLeaderboard")
+                .ToArray();
+
+            return data;
+        }
+    }
+}
