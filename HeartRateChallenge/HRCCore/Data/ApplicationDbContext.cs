862881d2f0a32a8563560c6163637fd6f74a545d
Blake Burgess <blake.burgess@maac.com>
2020-08-10 13:34:37 -0500

    HRC Core (Non Blazor) initial commit

---
+++ HeartRateChallenge/HRCCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HRCCore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Competitor> Competitors { get; set; }
+    }
+}
